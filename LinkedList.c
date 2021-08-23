87b07997d2f3932bdd0bcb60b2dfce6fda843e36
yalu-ku <74523709+yalu-ku@users.noreply.github.com>
2021-08-23 22:54:50 +0900

    linked list

---
+++ LinkedList.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define INF 10000
+
+int arr[INF];
+int count = 0;
+
+void addBack(int data) {
+    arr[count] = data;
+    count++;
+}
+
+void addFirst(int data) {
+    for (int i = count; i >= 1; i--) {
+        arr[i] = arr[i - 1]; //한칸씩 당겨주기
+    }
+    arr[0] = data; //첫번째 원소에 데이터 넣기
+    count++;
+}
+
+void show() {
+    for (int i=0; i<count; i++){
+        printf("%d\n", arr[i]);
+    }
+}
+
+int main(void) {
+    // printf("kdh\n");
+    addFirst(4);
+    addFirst(5);
+    addFirst(1);
+    addBack(7);
+    addBack(6);
+    addBack(8);
+    show();
+    return 0;
+}
